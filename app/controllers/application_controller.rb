cbbcbfa0b72b8fc06086cb6999c00dcadafb70ce
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-12 13:23:53 -0600

    Add rescue and errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found(message)
+    bad_params(message)
+  end
+
+  def bad_params(message)
+    body = ({
+      data: {},
+      error: "#{message}",
+      status: 404
+      }).to_json
+    render json: JSON.parse(body, :quirks_mode => true), status: 404
+  end
 end
