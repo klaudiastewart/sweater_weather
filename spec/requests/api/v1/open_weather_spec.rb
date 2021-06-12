b96b7895c69052fe61adc21e559f4cb1f5fcbd4c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-12 14:55:32 -0600

    Add sad path specs

--- spec/requests/api/v1/open_weather_spec.rb
+++ spec/requests/api/v1/open_weather_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe 'Open Weather API, api::v1::weathercontroller', type: :request do
       it 'can GET(forecast) current weather, daily weather, and hourly weather', :vcr do
         get "/api/v1/forecast", params: {location: "denver,co"}
         forecast = JSON.parse(response.body, symbolize_names: true)
-
         expect(response).to be_successful
         expect(forecast).to be_a(Hash)
         expect(forecast[:data].count).to eq(3)
         expect(forecast[:data][:type]).to eq("forecast")
         expect(forecast[:data][:attributes].keys).to eq([:current_weather, :daily_weather, :hourly_weather, :id])
 
+        expect(forecast[:data][:attributes][:current_weather].count).to_not eq(15)
         expect(forecast[:data][:attributes][:current_weather][:datetime]).to be_a(String)
         expect(forecast[:data][:attributes][:current_weather][:sunrise]).to be_a(String)
         expect(forecast[:data][:attributes][:current_weather][:sunset]).to be_a(String)
@@ -34,6 +34,8 @@ RSpec.describe 'Open Weather API, api::v1::weathercontroller', type: :request do
         expect(forecast[:data][:attributes][:current_weather][:conditions]).to be_a(String)
         expect(forecast[:data][:attributes][:current_weather][:icon]).to be_a(String)
 
+        expect(forecast[:data][:attributes][:daily_weather].count).to_not eq(8)
+        expect(forecast[:data][:attributes][:daily_weather].count).to eq(5)
         expect(forecast[:data][:attributes][:daily_weather].first[:date]).to be_a(String)
         expect(forecast[:data][:attributes][:daily_weather].first[:sunrise]).to be_a(String)
         expect(forecast[:data][:attributes][:daily_weather].first[:sunset]).to be_a(String)
@@ -42,10 +44,28 @@ RSpec.describe 'Open Weather API, api::v1::weathercontroller', type: :request do
         expect(forecast[:data][:attributes][:daily_weather].first[:conditions]).to be_a(String)
         expect(forecast[:data][:attributes][:daily_weather].first[:icon]).to be_a(String)
 
+        expect(forecast[:data][:attributes][:hourly_weather].count).to_not eq(48)
+        expect(forecast[:data][:attributes][:hourly_weather].count).to eq(8)
         expect(forecast[:data][:attributes][:hourly_weather].first[:time]).to be_a(String)
         expect(forecast[:data][:attributes][:hourly_weather].first[:temp]).to be_a(Float)
         expect(forecast[:data][:attributes][:hourly_weather].first[:conditions]).to be_a(String)
         expect(forecast[:data][:attributes][:hourly_weather].first[:icon]).to be_a(String)
+      end
+    end
+
+    describe 'sad path' do
+      it 'renders a 400 error if location is nil', :vcr do
+        get "/api/v1/forecast", params: {location: ""}
+        expect(response.status).to be(400)
+        forecast = JSON.parse(response.body, symbolize_names: true)
+        expect(forecast[:error]).to eq("Location not found")
+      end
+
+      it 'renders a 400 error if location is not present', :vcr do
+        get "/api/v1/forecast"
+        expect(response.status).to be(400)
+        forecast = JSON.parse(response.body, symbolize_names: true)
+        expect(forecast[:error]).to eq("Location not found")
       end
     end
   end
