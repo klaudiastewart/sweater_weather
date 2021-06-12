8a5381669d36f27ffe35d700f8c5e98ef99949d0
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-12 13:23:36 -0600

    Add record not found sad path and forecast serializer

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::WeatherController < ApplicationController
   def forecast
     if params[:location].nil? || !params[:location].present?
-      # render 400 or 404
+      return record_not_found("Record not found")
     else
       @forecast = WeatherFacade.get_forecast(params[:location])
+      render json: ForecastSerializer.new(@forecast)
     end
   end
 end
