4f3514cfed409627c3754ac0f25298a394f98783
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-12 14:55:06 -0600

    Change sad path

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::WeatherController < ApplicationController
   def forecast
     if params[:location].nil? || !params[:location].present?
-      return record_not_found("Record not found")
+      return bad_params_400("Location not found")
     else
       @forecast = WeatherFacade.get_forecast(params[:location])
       render json: ForecastSerializer.new(@forecast)
