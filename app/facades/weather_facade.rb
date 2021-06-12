a41025b75e88a58ae5cf08d797697c4cdf5736b3
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-11 20:18:06 -0600

    Get current weather info

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,8 +1,33 @@
 class WeatherFacade
   class << self
     def get_forecast(location)
-      lat_long = MapquestService.get_lat_long(location)
-      require "pry"; binding.pry
+      lat_lon = MapquestService.get_lat_long(location)
+      lat = lat_lon[:results].first[:locations].first[:latLng][:lat]
+      lon = lat_lon[:results].first[:locations].first[:latLng][:lng]
+      forecast = OpenweatherService.get_forecast(lat, lon)
+
+      current_weather = {
+        datetime: Time.at(forecast[:current][:dt]).to_s,
+        sunrise: Time.at(forecast[:current][:sunrise]).to_s,
+        sunset: Time.at(forecast[:current][:sunset]).to_s,
+        temperature: forecast[:current][:temp],
+        feels_like: forecast[:current][:feels_like],
+        humidity: forecast[:current][:humidity],
+        uvi: forecast[:current][:uvi],
+        visibility: forecast[:current][:visibility],
+        conditions: forecast[:current][:weather].first[:description],
+        icon: forecast[:current][:weather].first[:icon]
+      }
+
+      daily_weather = {
+
+      }
+
+      hourly_weather = {
+
+      }
+      
+      Forecast.new(forecast)
     end
   end
 end
