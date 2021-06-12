23a69e1573e4c453549c9d0081fb59f1b10836e0
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-12 13:24:39 -0600

    Done with weather facade, need refactor later

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -18,19 +18,30 @@ class WeatherFacade
         conditions: forecast[:current][:weather].first[:description],
         icon: forecast[:current][:weather].first[:icon]
       }
-
-      require "pry"; binding.pry
       daily_weather = forecast[:daily].map do |day|
         {
-
+          date: Time.at(day[:dt]).to_date,
+               sunrise: Time.at(day[:sunrise]).to_s(:time),
+               sunset: Time.at(day[:sunset]).to_s(:time),
+               max_temp: day[:temp][:max],
+               min_temp: day[:temp][:min],
+               conditions: day[:weather].first[:description],
+               icon: day[:weather].first[:icon]
         }
       end
-      
-      hourly_weather = {
+      hourly_weather = forecast[:hourly].map do |hour|
+        {
+          time: Time.at(hour[:dt]).to_s(:time),
+          temp: hour[:temp],
+          conditions: hour[:weather].first[:description],
+          icon: hour[:weather].first[:icon]
+        }
+      end
 
-      }
+      daily = daily_weather[0..4]
+      hourly = hourly_weather[0..7]
 
-      # Forecast.new(forecast)
+      Forecast.new(current_weather, daily, hourly)
     end
   end
 end
