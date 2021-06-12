2aadd84f79771a400565ca03c2fc03f356f8c4d9
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-11 19:20:57 -0600

    Add get lat long by location method and connector

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,23 @@
+class MapquestService
+  class << self
+    def get_lat_long(location)
+      resp = conn.get("/geocoding/v1/address") do |faraday|
+        faraday.params['location'] = location
+      end
+      parse_json(resp)
+    end
+
+    private
+
+    def conn
+      Faraday.new(
+        url: "http://www.mapquestapi.com",
+        params: { key: ENV['MAPQUEST_KEY']}
+      )
+    end
+
+    def parse_json(resp)
+      JSON.parse(resp.body, symbolize_names: true)
+    end
+  end
+end
