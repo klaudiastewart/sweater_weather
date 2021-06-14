57b5193c62b94568e778d6f297881de3536a9e56
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-13 20:06:39 -0600

    Update spec

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Mapquest Service' do
   describe 'calls to the API' do
     it 'can get_lat_long for a given location', :vcr do
       call = MapquestService.get_lat_long('denver,co')
-      expect(call.response).to eq(200)
       expect(call.class).to eq(Hash)
     end
   end
