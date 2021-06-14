226d2a095861b31b5103401fd2f92d1e7b83d4f6
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 15:20:04 -0600

    Add users session create route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'weather#forecast'
       get '/backgrounds', to: 'cityimage#background'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'users/sessions#create'
 
       get '/book-search', to: 'books#search'
     end
