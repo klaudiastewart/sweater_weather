fa46043dcdd57447b485640e513a851b50051344
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 11:00:41 -0600

    Fix up tests

--- spec/requests/api/v1/books_controller_spec.rb
+++ spec/requests/api/v1/books_controller_spec.rb
@@ -13,47 +13,47 @@ RSpec.describe 'Books Controller, api::v1::Bookscontroller', type: :request do
   describe 'GET /book-search' do
     describe 'happy path' do
       it 'can get destination, forecast, total books found, and books for a get /book-search request', :vcr do
-        get "/api/v1/books-search", params: {q: "denver,co", quantity: 5}
-        response = JSON.parse(response.body, symbolize_names: true)
+        get "/api/v1/book-search", params: {location: "denver,co", quantity: 5}
+        book = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
-        expect(image).to be_a(Hash)
-        expect(image[:data].count).to eq(3)
-        expect(image[:data][:type]).to eq("books")
-        expect(image[:data][:attributes].count).to eq(4)
-        expect(image[:data][:attributes].keys).to eq([:destination, :forecast, :total_books_found, :books])
+        expect(book).to be_a(Hash)
+        expect(book[:data].count).to eq(3)
+        expect(book[:data][:type]).to eq("book")
+        expect(book[:data][:attributes].count).to eq(4)
+        expect(book[:data][:attributes].keys).to eq([:destination, :forecast, :books_found, :books])
 
-        expect(image[:data][:attributes][:destination]).to be_a(String)
-        expect(image[:data][:attributes][:forecast]).to be_a(Hash)
-        expect(image[:data][:attributes][:total_books_found]).to be_a(Integer)
-        expect(image[:data][:attributes][:books]).to be_a(Array)
+        expect(book[:data][:attributes][:destination]).to be_a(String)
+        expect(book[:data][:attributes][:forecast]).to be_a(Hash)
+        expect(book[:data][:attributes][:books_found]).to be_a(Integer)
+        expect(book[:data][:attributes][:books]).to be_a(Array)
 
-        expect(image[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
-        expect(image[:data][:attributes][:forecast][:summary]).to be_a(String)
-        expect(image[:data][:attributes][:forecast][:temperature]).to be_a(String)
+        expect(book[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+        expect(book[:data][:attributes][:forecast][:summary]).to be_a(String)
+        expect(book[:data][:attributes][:forecast][:temperature]).to be_a(Float)
 
-        expect(image[:data][:attributes][:books].count).to eq(5)
-        expect(image[:data][:attributes][:books].first.keys).to eq([:isbn, :title, :publisher])
-        expect(image[:data][:attributes][:books].first[:isbn].count).to eq(2)
-        expect(image[:data][:attributes][:books].first[:title]).to be_a(String)
-        expect(image[:data][:attributes][:books].first[:publisher].count).to eq(1)
-        expect(image[:data][:attributes][:books].first[:publisher].first).to be_a(String)
+        expect(book[:data][:attributes][:books].count).to eq(5)
+        expect(book[:data][:attributes][:books].first.keys).to eq([:isbn, :title, :publisher])
+        expect(book[:data][:attributes][:books].first[:isbn].count).to eq(2)
+        expect(book[:data][:attributes][:books].first[:title]).to be_a(String)
+        expect(book[:data][:attributes][:books].first[:publisher].count).to eq(1)
+        expect(book[:data][:attributes][:books].first[:publisher].first).to be_a(String)
       end
     end
 
     describe 'sad path' do
       it 'renders a 400 error if q and query is empty and quantity is 0', :vcr do
-        get "/api/v1/books-search", params: {q: "", quantity: 0}
+        get "/api/v1/book-search", params: {location: "", quantity: 0}
         expect(response.status).to be(400)
-        response = JSON.parse(response.body, symbolize_names: true)
-        expect(response[:error]).to eq("Query not found")
+        book = JSON.parse(response.body, symbolize_names: true)
+        expect(book[:error]).to eq("Book not found")
       end
 
       it 'renders a 400 error if quantity and query is not present', :vcr do
-        get "/api/v1/books-search"
+        get "/api/v1/book-search"
         expect(response.status).to be(400)
-        response = JSON.parse(response.body, symbolize_names: true)
-        expect(response[:error]).to eq("Query not found")
+        book = JSON.parse(response.body, symbolize_names: true)
+        expect(book[:error]).to eq("Book not found")
       end
     end
   end
