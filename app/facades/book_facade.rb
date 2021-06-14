fc123413e0b03088ec95263f6c3db903c073f9db
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 10:08:41 -0600

    Update book facade and add map method for returned books

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,18 +1,30 @@
 class BookFacade
   class << self
     def get_books(location, quantity)
-      require "pry"; binding.pry
       lat_lon = MapquestService.get_lat_long(location)
       lat = lat_lon[:results].first[:locations].first[:latLng][:lat]
       lon = lat_lon[:results].first[:locations].first[:latLng][:lng]
+      destination  = lat_lon[:results].first[:providedLocation][:location]
       weather = OpenweatherService.get_forecast(lat, lon)
       forecast = {
         summary: weather[:current][:weather].first[:description],
         temperature: (weather[:current][:temp] - 273) * 1.8 + 32
       }
-      require "pry"; binding.pry
-      # books = OpenlibraryService.find_books(book)
-      # Book.new(books)
+      searched_books = OpenlibraryService.find_books(location, quantity)
+      Book.new(destination, forecast, searched_books[:numFound], books_hash(searched_books))
+    end
+
+    def books_hash(searched_books)
+      books = searched_books[:docs].map do |book|
+        [
+          {
+          isbn: book[:isbn],
+          title: book[:title],
+          publisher: book[:publisher]
+        }
+      ]
+      end
+      books.first(5)
     end
   end
 end
