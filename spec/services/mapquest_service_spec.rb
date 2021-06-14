1598958fad040910e99542eb5a6eb1fdec2a3a60
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-13 19:35:36 -0600

    Add mapquest spec

---
+++ spec/services/mapquest_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Mapquest Service' do
+  describe 'calls to the API' do
+    it 'can get_lat_long for a given location', :vcr do
+      call = MapquestService.get_lat_long('denver,co')
+      expect(call.response).to eq(200)
+      expect(call.class).to eq(Hash)
+    end
+  end
+end
