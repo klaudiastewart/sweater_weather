c9f4b629b7598726f136c28d3efd207984046167
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 15:19:48 -0600

    Add 401 bad params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  # def record_not_found(message)
-  #   bad_params(message)
-  # end
-
+  
   def bad_params_400(message)
     body = ({
       error: "#{message}",
       status: 400
       }).to_json
     render json: JSON.parse(body, :quirks_mode => true), status: 400
+  end
+
+  def bad_params_401(message)
+    body = ({
+      error: "#{message}",
+      status: 401
+      }).to_json
+    render json: JSON.parse(body, :quirks_mode => true), status: 401
   end
 end
