aea078f8e62e8004e1d28d33ab3b3d614ae09ee8
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 11:34:40 -0600

    Add books_hash method

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Book Facade' do
         @lat_lng[:results][0][:locations][0][:latLng][:lat],
         @lat_lng[:results][0][:locations][0][:latLng][:lng]
       )
+      @searched_books = OpenlibraryService.find_books('denver,co', 5)
     end
 
     it 'gets current weather, daily weather, and hourly weather to make a forecast object', :vcr do
@@ -18,6 +19,14 @@ RSpec.describe 'Book Facade' do
     it 'gets books based on location', :vcr do
       response = BookFacade.get_books('denver, co', 5)
       expect(response.class).to eq(Book)
+    end
+
+    it 'gets books_hash', :vcr do
+      response = BookFacade.books_hash(@searched_books)
+      expect(response.class).to eq(Array)
+      expect(response.count).to eq(5)
+      expect(response.first.keys).to eq([:isbn, :title, :publisher])
+      expect(response.first.keys.count).to eq(3)
     end
   end
 end
