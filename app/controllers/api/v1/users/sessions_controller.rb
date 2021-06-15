7d302a13ea19cb93891dbff2e3ccc18c1e7741e6
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-14 20:10:53 -0600

    Fix sad path logic

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Users::SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
-    if @user == @user.authenticate(params[:password])
+    if !@user.present? || !@user.authenticate(params[:password])
+      return bad_params_401("No authorization")
+    else
       user_session = SessionsFacade.post_user_session(@user)
       render json: UserSerializer.new(user_session)
-    else
-      return bad_params_401("No authorization")
     end
   end
 end
