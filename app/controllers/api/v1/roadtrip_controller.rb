3b86456dedbe4dbd5cdb8871e2f0a7068a22ca52
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-15 19:33:54 -0600

    Account for sad path

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::RoadtripController < ApplicationController
   def index
-    # require "pry"; binding.pry
-    if !params[:origin].present? || !params[:destination].present?  || !params[:api_key].present?
-      return bad_params_400("Missing params")
+    if !params[:origin].present? || !params[:destination].present?
+      return bad_params_400("Missing or bad params")
+    elsif !params[:api_key].present? || User.find_by_api_key(params[:api_key]).nil?
+      return bad_params_401("Unauthorized")
     else
       trip = RoadtripFacade.get_road_trip(params[:origin], params[:destination])
+      return bad_params_400(trip) if trip.class == String
+      render json: RoadtripSerializer.new(trip)
     end
   end
 end
